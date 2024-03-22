30ed3c9f76971028f029b99836acaea4bc720d41
abdallah-abde <abdallah.abde@gmail.com>
2024-03-22 11:36:21 +0300

    use effect examples

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import EffectComponent from "./components/EffectComponent";
+import EffectComponent2 from "./components/EffectComponent2";
 import StateComponent from "./components/StateComponent";
 
 function App() {
   return (
     <>
-      <StateComponent />
+      {/* <StateComponent /> */}
+      {/* <EffectComponent /> */}
+      <EffectComponent2 />
     </>
   );
 }
---
+++ src/components/EffectComponent2.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+export default function EffectComponent() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <div>{windowWidth}</div>;
+}
